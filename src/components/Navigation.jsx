24a81c0b22a3cbeca330dc0f8b3331f54db9ea3d
Jamilur Rahman <r.jamilur96@gmail.com>
2024-12-12 23:51:43 -0500

    navlogo fix

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Navigation() {
   return (
     <nav className="nav">
-      <img src="/pokemon-logo.png" alt="Pokemon Logo" className="nav-logo" />
+      <img src="./pokemon-logo.png" alt="Pokemon Logo" className="nav-logo" />
       <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/media">Media</Link>
